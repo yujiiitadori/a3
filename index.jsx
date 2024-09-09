a6a1b1483c62895afdd428166366af135f01bc86
yujiiitadori <132886066+yujiiitadori@users.noreply.github.com>
2024-09-09 19:42:33 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,47 +1,93 @@
-import { createContext, useState } from "react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import TransactionForm from "../add-transaction";
+import TransactionChartSummary from "../chart";
 
-export const GlobalContext = createContext(null);
-
-export default function GlobalState({ children }) {
-  const [formData, setFormData] = useState({
-    type: "income",
-    amount: 0,
-    description: "",
-  });
-
-  const [value, setValue] = useState("expense");
-  const [totalExpense, setTotalExpense] = useState(0);
-  const [totalIncome, setTotalIncome] = useState(0);
-  const [allTransactions, setAllTransactions] = useState([]);
-
-  function handleFormSubmit(currentFormData) {
-    if (!currentFormData.description || !currentFormData.amount) return;
-
-    setAllTransactions([
-      ...allTransactions,
-      { ...currentFormData, id: Date.now() },
-    ]);
-  }
-
-  console.log(allTransactions);
-
+export default function Summary({
+  onClose,
+  isOpen,
+  totalExpense,
+  totalIncome,
+}) {
   return (
-    <GlobalContext.Provider
-      value={{
-        formData,
-        setFormData,
-        totalExpense,
-        setTotalExpense,
-        totalIncome,
-        setTotalIncome,
-        value,
-        setValue,
-        allTransactions,
-        setAllTransactions,
-        handleFormSubmit,
-      }}
+    <Box
+      p="6"
+      border={"1px solid"}
+      borderColor={"gray.100"}
+      overflow={"hidden"}
+      borderRadius={"10"}
+      background={"white"}
+      display={"flex"}
     >
-      {children}
-    </GlobalContext.Provider>
+      <Flex
+        w="full"
+        justifyContent={"center"}
+        alignItems={"center"}
+        flexDirection={{
+          base: "column",
+          sm: "column",
+          md: "column",
+          lg: "row",
+          xl: "row",
+        }}
+      >
+        <Flex
+          flex={1}
+          w={"full"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"space-evenly"}
+          ml={"-20"}
+          mr={"2"}
+        >
+          <Heading size={"md"} mb={"4"} color={"gray.600"}>
+            Balance is $ {totalIncome - totalExpense}
+          </Heading>
+          <Flex
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            bg={"gray.50"}
+            w="full"
+            h="100px"
+            border={"1px solid"}
+            borderColor={"gray.100"}
+          >
+            <Flex flexDirection={"column"}>
+              <Heading color={"gray.700"}>$ {totalIncome}</Heading>
+              <Text color={"gray.600"}>Total Income</Text>
+            </Flex>
+          </Flex>
+          <Flex
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            bg={"gray.50"}
+            w="full"
+            h="100px"
+            border={"1px solid"}
+            borderColor={"gray.100"}
+          >
+            <Flex flexDirection={"column"}>
+              <Heading color={"gray.700"}>$ {totalExpense}</Heading>
+              <Text color={"gray.600"}>Total Expense</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Box
+          flex={1}
+          mt={"10"}
+          ml={"-90px"}
+          mr={"5"}
+          width={"300px"}
+          height={"300px"}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Heading>
+            <TransactionChartSummary expense={totalExpense} income={totalIncome} />
+          </Heading>
+        </Box>
+      </Flex>
+      <TransactionForm onClose={onClose} isOpen={isOpen} />
+    </Box>
   );
 }
